396ab3414d67125c33a561e07d61345df8d0cb14
jb67396 <john.ballock@utexas.edu>
2018-04-28 17:26:31 -0500

    Refixing bugs and adding some

--- LED.c
+++ LED.c
@@ -38,19 +38,18 @@
 // Initialize GPIO Port F for negative logic switches on PF0 and
 // PF4 as the Launchpad is wired.  Weak internal pull-up
 // resistors are enabled, and the NMI functionality on PF0 is disabled.
-// LEDs on PF3,2,1 are enabled
+// LEDs on PF3,2,1,0 are enabled
 // Input: none
 // Output: none
 void LED_Init(void){       
-  SYSCTL_RCGCGPIO_R |= 0x20;     // 1) activate Port F
-  while((SYSCTL_PRGPIO_R & 0x20)!=0x20){}; // wait to finish activating     
+	SYSCTL_RCGCGPIO_R |= 0x20;     // 1) activate Port F
+  while((SYSCTL_PRGPIO_R & 0x20)== 0){}; // wait to finish activating     
   GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;// 2a) unlock GPIO Port F Commit Register
   GPIO_PORTF_CR_R = 0x0F;        // 2b) enable commit for PF4-PF0     
-  GPIO_PORTF_AMSEL_R &= ~0x0F;   // 3) disable analog functionality on PF4-PF0     
+  GPIO_PORTF_AMSEL_R = 0x00;   // 3) disable analog functionality on PF4-PF0     
   GPIO_PORTF_PCTL_R = 0x00000000;// 4) configure PF0-PF4 as GPIO
   GPIO_PORTF_DIR_R = 0x0F;       // 5) make PF3-0 output                        
   GPIO_PORTF_AFSEL_R = 0;        // 6) disable alt funct
-  GPIO_PORTF_PUR_R = 0x00;       // enable weak pull-up on none
   GPIO_PORTF_DEN_R = 0x0F;       // 7) enable digital I/O on PF0-PF4
 }
 
@@ -59,21 +58,21 @@ void LED_Init(void){
 // Input: none
 // Output: none
 void LED_RightOn(void){
-  GPIO_PORTF_DATA_R = 0x0A;
+  GPIO_PORTF_DATA_R |= 0x05;
 }
 //------------LED_RightOff------------
 // Turn off right side LED's
 // Input: none
 // Output: none
 void LED_RightOff(void){
-  GPIO_PORTF_DATA_R = 0x00;
+  GPIO_PORTF_DATA_R &= ~0x05;
 }
 //------------LED_RightToggle------------
 // Toggle right side LED's
 // Input: none
 // Output: none
 void LED_RightToggle(void){
-  GPIO_PORTF_DATA_R ^= 0x0A;
+  GPIO_PORTF_DATA_R ^= 0x05;
 }
 
 //------------LED_LeftToggle------------
@@ -81,21 +80,21 @@ void LED_RightToggle(void){
 // Input: none
 // Output: none
 void LED_LeftToggle(void){
-  GPIO_PORTF_DATA_R ^= 0x05;
+  GPIO_PORTF_DATA_R ^= 0x0A;
 }
 //------------LED_LeftOn------------
 // Turn on left side LED's
 // Input: none
 // Output: none
 void LED_LeftOn(void){
-  GPIO_PORTF_DATA_R = 0x05;
+  GPIO_PORTF_DATA_R |= 0x0A;
 }
 //------------LED_LeftOff------------
 // Turn off left side LED's
 // Input: none
 // Output: none
 void LED_LeftOff(void){
-  GPIO_PORTF_DATA_R = 0x00;
+  GPIO_PORTF_DATA_R &= ~0x0A;
 }
 
 
